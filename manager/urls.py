9e6ea7cf1350034a88e5f78f8135e0e9401042b9
Umesh Mandal <uk.mandal667@hotmail.com>
2024-11-19 20:24:44 +0900

    version 0.0.4

--- manager/urls.py
+++ manager/urls.py
@@ -5,12 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:pk>/', views.manager_detail, name='manager_detail'),
-
-    
-    # path('', views.index, name='index'),
-    # path('<int:id>', views.view_student, name='view_student'),
-    # path('add/', views.add, name='add'),
-    # path('edit/<int:id>/', views.edit, name='edit'),
-    # path('delete/<int:id>/', views.delete, name='delete'),
+    path('<int:pk>/', views.manager_detail, name='manager_detail')
 ]
